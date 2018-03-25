bf95784b598a5502c0637a7be60850c9df0bb606
wendyhoy <wendy.hoy@gmail.com>
2018-03-25 10:57:56 -0700

    Add basic NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
 import HomePage from './HomePage';
 import TeamShowPage from './TeamShowPage';
 import UserShowPage from './UserShowPage';
@@ -10,12 +11,15 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/teams/:id" component={TeamShowPage}/>
-          <Route path="/users/:id" component={UserShowPage}/>
-          <Route path="/signing_in" component={SigningInPage}/>
-        </Switch>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="/teams/:id" component={TeamShowPage}/>
+            <Route path="/users/:id" component={UserShowPage}/>
+            <Route path="/signing_in" component={SigningInPage}/>
+          </Switch>
+        </div>
       </Router>
     );
   }
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+function NavBar () {
+  return(
+    <nav className="uk-navbar-container uk-light" uk-navbar="">
+      <div className="uk-navbar-left">
+        <ul className="uk-navbar-nav">
+          <li>
+            <NavLink className="uk-navbar-item uk-logo" exact to="/">
+              <img src="/images/logo-64.png" width="50px" className="uk-margin-small-right" alt="shokubot logo" />
+              shokubot
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="uk-navbar-right">
+        <ul className="uk-navbar-nav">
+          <li><NavLink className="uk-navbar-item" exact to={`/users/44`}>My Profile</NavLink></li>
+          <li><NavLink className="uk-navbar-item" exact to={`/teams/14`}>My Team</NavLink></li>
+        </ul>        
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
